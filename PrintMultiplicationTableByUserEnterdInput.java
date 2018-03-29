2d9e98a2893070160adf727c52965a8abc2b6cb2
himavanth <depahimavanth@gmail.com>
2018-03-29 22:58:20 +0530

    29 march

---
+++ PrintMultiplicationTableByUserEnterdInput.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class PrintMultiplicationTableByUserEnterdInput {
+    public static void main(String args[]) {
+
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        for (int i = 1; i <= 10; i++)
+            System.out.println(n + " x " + i + " = " + (n * i));
+
+    }
+}
+
+
